4a61c6b520d566485d405eab589391cba84f5bca
elijahnacc <elijah.newlon@g.austincc.edu>
2025-05-16 03:24:46 +0000

    question 4

--- src/come_out_phase.cpp
+++ src/come_out_phase.cpp
@@ -1,1 +1,12 @@
-//
+#include "phase.h"
+#include "come_out_phase.h"
+
+RollOutcome ComeOutPhase::get_outcome(Roll* roll) {
+
+    if (roll -> roll_value() == 7 || roll -> roll_value() == 11)
+        return RollOutcome::natural;
+    else if (roll -> roll_value() == 2 || roll -> roll_value() == 3 || roll -> roll_value() == 12)
+        return RollOutcome::craps;
+    else
+        return RollOutcome::point;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,6 +3,10 @@
 #include "die.h"
 #include "roll.h"
 #include "shooter.h"
+#include "phase.h"
+#include "point_phase.h"
+#include "come_out_phase.h"
+#include <vector>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -48,4 +52,71 @@ TEST_CASE("Test Shooter class") {
 		CHECK(current_roll <= 12);
 		CHECK(current_roll >= 2);
 	}
+}
+
+TEST_CASE("Test ComeOutPhase get_outcome()") {
+
+	Die uno;
+	Die dos;
+	ComeOutPhase phase;
+
+	std::vector<Roll*> rolls;
+	bool get_natural = false, get_craps = false, get_point = false;
+
+	do {
+		Roll roll {uno, dos};
+		roll.roll_dice();
+		rolls.push_back(&roll);
+		
+		if (roll.roll_value() == 7 || roll.roll_value() == 11) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::natural);
+			get_natural = true;
+		}
+		else if (roll.roll_value() == 2 || roll.roll_value() == 3 || roll.roll_value() == 12) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::craps);
+			get_craps = true;
+		}
+		else {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::point);
+			get_point = true;
+		}
+
+		if (rolls.size() >= 24)
+			break;
+		
+	} while (!get_natural || !get_craps || !get_point);
+}
+
+TEST_CASE("Test PointPhase get_outcome()") {
+
+	Die uno;
+	Die dos;
+	int point = 5;
+	PointPhase phase(point);
+
+	std::vector<Roll*> rolls;
+	bool get_point = false, get_sevenout = false, get_nopoint = false;
+
+	do {
+		Roll roll {uno, dos};
+		roll.roll_dice();
+		rolls.push_back(&roll);
+
+		if (roll.roll_value() == 7) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::seven_out);
+			get_sevenout = true;
+		}
+		else if (roll.roll_value() == point) {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::point);
+			get_point = true;
+		}
+		else {
+			REQUIRE(phase.get_outcome(&roll) == RollOutcome::nopoint);
+			get_nopoint = true;
+		}
+
+		if (rolls.size() >= 24)
+			break;
+
+	} while (!get_point || !get_sevenout || !get_nopoint);
 }
